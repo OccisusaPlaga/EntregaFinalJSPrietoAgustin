bed6e15c5ccb5a3a806e2e08b80e04688f6cc40a
Prieto Agustin <prietoagustinivan52@gmail.com>
2023-08-22 13:12:59 -0300

    estoy re quemado me faltaba casi todo

--- consignas.js
+++ consignas.js
@@ -1,23 +1,23 @@
 const menu0 = ""
-+"MenuPrincipal\n"
-+"1Comprar animal marino\n"
-+"2Ver Precio Total\n"
-+"3Ver Descuentos Disponibles\n"
-+"0Salir\n"
++"-MenuPrincipal\n"
++"1-Comprar animal marino\n"
++"2-Ver Precio Total\n"
++"3-Ver Descuentos Disponibles\n"
++"0-Salir\n"
 
 const menu1= ""
-+"Objetos\n"
-+"1Pez Payaso-150$\n"
-+"2Tortuga-200$\n"
-+"3Pulpo-450$\n"
-+"4Calamar-480$\n"
-+"0Volver\n"
-
++"-Peces\n"
++"1-Pez Payaso-150$\n"
++"2-Tortuga-200$\n"
++"3-Pulpo-450$\n"
++"4-Calamar-480$\n"
++"0-Volver\n"
++"s-cerrar\n"
 
 const menu2=""
-+"Comprando 4 10%off\n"
-+"Comprando 6 15%off\n"
-+"comprando 8 25%off\n"
++"-Comprando 4 10%off\n"
++"-Comprando 6 15%off\n"
++"-comprando 8 25%off\n"
 
 let Precio = 0;
 let PrecioVar = 0;
@@ -28,7 +28,7 @@ function SumarProductos(Precio){
     PrecioVar += Precio;
     ProdTotales++;
 
-    alert("¡Agregaste un pez al carrito!\n\nPrecio del preducto: $"+Precio+".\n\nPrecio total de la compra: $"+PrecioVar+".\nCantidad de peces: "+ProdTotales+". Descuento aplicado: "+descuentos+"")
+    return "¡Agregaste un pez al carrito!\n\nPrecio del preducto: $"+Precio+".\n\nPrecio total de la compra: $"+PrecioVar+".\nCantidad de peces: "+ProdTotales+". Descuento aplicado: "+descuentos+"";
 
 }
 
@@ -36,9 +36,9 @@ function calcularDescuento(){
     if (ProdTotales >= 4 && ProdTotales < 6)
         descuentos = 10;
     else if(ProdTotales >= 6 && ProdTotales <8)
-    descuentos = 15;
+        descuentos = 15;
     else if(ProdTotales >=8 )
-    descuentos= 25;
+        descuentos= 25;
 }
 
 function PrecioFinal(){
@@ -48,11 +48,53 @@ function PrecioFinal(){
     if (descuentos == 0)
     {
         Total = PrecioVar;
-        alert("El Precio Final de su compra es $"+Total+".\n\nLa compra no tiene descuentos.")
+        return "El Precio Final de su compra es $"+Total+".\n\nLa compra no tiene descuentos.";
     }
     else
     {
         Total = PrecioVar - ((descuentos/100)* PrecioVar);
-        alert("El precio final de su compra es "+Total+".\n\nLa compra tiene un descuento del "+descuentos+"%")
+        return "El precio final de su compra es "+Total+".\n\nLa compra tiene un descuento del "+descuentos+"%";
     }
 }
+
+var texto = 0;
+do {
+    texto = parseInt(prompt(menu0));
+    switch (texto) {
+        case 1:
+            do {
+                texto = parseInt(prompt(menu1));
+                switch (texto) {
+                    case 1:
+                        alert(SumarProductos(150));
+                        break;
+                    case 2:
+                        alert(SumarProductos(200));
+                        break;
+                    case 3:
+                        alert(SumarProductos(450));
+                        break;
+                    case 4:
+                        alert(SumarProductos(480));
+                        break;
+                }
+            } while (texto !== 0);
+            break;
+        case 2:
+            alert("PrecioFinal()");
+            break;
+        case 3:
+            do{
+                texto= parseInt(prompt(menu2));
+                switch (texto){
+                case 1:
+                }
+            }while(texto!==0)
+            break;
+        case 0:
+            alert("!Vuelva Pronto!");
+            break;
+        default:
+            alert("El código no existe");
+    }
+} while (texto !== 0);
