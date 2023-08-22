661d7f1f3d47c1b370a582866f48571bb2563fab
Prieto Agustin <prietoagustinivan52@gmail.com>
2023-08-22 10:32:58 -0300

    teoricamente deberia andar

--- consignas.js
+++ consignas.js
@@ -13,18 +13,46 @@ const menu1= ""
 +"4Calamar-480$\n"
 +"0Volver\n"
 
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
-+"\n"
+
+const menu2=""
++"Comprando 4 10%off\n"
++"Comprando 6 15%off\n"
++"comprando 8 25%off\n"
+
+let Precio = 0;
+let PrecioVar = 0;
+let ProdTotales = 0;
+let descuentos = 0;
+
+function SumarProductos(Precio){
+    PrecioVar += Precio;
+    ProdTotales++;
+
+    alert("¡Agregaste un pez al carrito!\n\nPrecio del preducto: $"+Precio+".\n\nPrecio total de la compra: $"+PrecioVar+".\nCantidad de peces: "+ProdTotales+". Descuento aplicado: "+descuentos+"")
+
+}
+
+function calcularDescuento(){
+    if (ProdTotales >= 4 && ProdTotales < 6)
+        descuentos = 10;
+    else if(ProdTotales >= 6 && ProdTotales <8)
+    descuentos = 15;
+    else if(ProdTotales >=8 )
+    descuentos= 25;
+}
+
+function PrecioFinal(){
+    let Total=0;
+
+    calcularDescuento();
+    if (descuentos == 0)
+    {
+        Total = PrecioVar;
+        alert("El Precio Final de su compra es $"+Total+".\n\nLa compra no tiene descuentos.")
+    }
+    else
+    {
+        Total = PrecioVar - ((descuentos/100)* PrecioVar);
+        alert("El precio final de su compra es "+Total+".\n\nLa compra tiene un descuento del "+descuentos+"%")
+    }
+}
